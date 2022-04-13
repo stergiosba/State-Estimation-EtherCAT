e14605e23daa8514a7990344269343801bc53e34
Stergios Bachoumas <stergioba@gmail.com>
2022-04-14 02:53:57 +0300

    Version 1.0 Cleanup

--- plotter/myCHOLMATLAB.m
+++
@@ -1,8 +0,0 @@
-function [L, invL] = myCHOLMATLAB(A)
-
-n = size(A,1);
-L=zeros(n);
-
-L = chol(A,'lower');
-
-invL = inv(L);
--- speed_calc/matmul.c
+++
@@ -1,9 +0,0 @@
-static inline void matmul(m_elem Pxx_[M], m_elem Jfx[][N], m_elem Q[M], m_elem Pxx[M])
-{
-	Pxx[0] = Q[0] + Jfx[0][0]*(Pxx_[0]*Jfx[0][0]+Pxx_[1]*Jfx[0][1]+Pxx_[2]*Jfx[0][2])+Jfx[0][1]*(Pxx_[1]*Jfx[0][0]+Pxx_[3]*Jfx[0][1]+Pxx_[4]*Jfx[0][2])+Jfx[0][2]*(Pxx_[2]*Jfx[0][0]+Pxx_[4]*Jfx[0][1]+Pxx_[5]*Jfx[0][2]);
-	Pxx[1] = Q[1] + Jfx[0][0]*(Pxx_[0]*Jfx[1][0]+Pxx_[1]*Jfx[1][1]+Pxx_[2]*Jfx[1][2])+Jfx[0][1]*(Pxx_[1]*Jfx[1][0]+Pxx_[3]*Jfx[1][1]+Pxx_[4]*Jfx[1][2])+Jfx[0][2]*(Pxx_[2]*Jfx[1][0]+Pxx_[4]*Jfx[1][1]+Pxx_[5]*Jfx[1][2]);
-	Pxx[2] = Q[2] + Jfx[0][0]*(Pxx_[0]*Jfx[2][0]+Pxx_[1]*Jfx[2][1]+Pxx_[2]*Jfx[2][2])+Jfx[0][1]*(Pxx_[1]*Jfx[2][0]+Pxx_[3]*Jfx[2][1]+Pxx_[4]*Jfx[2][2])+Jfx[0][2]*(Pxx_[2]*Jfx[2][0]+Pxx_[4]*Jfx[2][1]+Pxx_[5]*Jfx[2][2]);
-	Pxx[3] = Q[3] + Jfx[1][0]*(Pxx_[0]*Jfx[1][0]+Pxx_[1]*Jfx[1][1]+Pxx_[2]*Jfx[1][2])+Jfx[1][1]*(Pxx_[1]*Jfx[1][0]+Pxx_[3]*Jfx[1][1]+Pxx_[4]*Jfx[1][2])+Jfx[1][2]*(Pxx_[2]*Jfx[1][0]+Pxx_[4]*Jfx[1][1]+Pxx_[5]*Jfx[1][2]);
-	Pxx[4] = Q[4] + Jfx[1][0]*(Pxx_[0]*Jfx[2][0]+Pxx_[1]*Jfx[2][1]+Pxx_[2]*Jfx[2][2])+Jfx[1][1]*(Pxx_[1]*Jfx[2][0]+Pxx_[3]*Jfx[2][1]+Pxx_[4]*Jfx[2][2])+Jfx[1][2]*(Pxx_[2]*Jfx[2][0]+Pxx_[4]*Jfx[2][1]+Pxx_[5]*Jfx[2][2]);
-	Pxx[5] = Q[5] + Jfx[2][0]*(Pxx_[0]*Jfx[2][0]+Pxx_[1]*Jfx[2][1]+Pxx_[2]*Jfx[2][2])+Jfx[2][1]*(Pxx_[1]*Jfx[2][0]+Pxx_[3]*Jfx[2][1]+Pxx_[4]*Jfx[2][2])+Jfx[2][2]*(Pxx_[2]*Jfx[2][0]+Pxx_[4]*Jfx[2][1]+Pxx_[5]*Jfx[2][2]);
-}
