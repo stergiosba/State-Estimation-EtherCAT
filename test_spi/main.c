4aec1d1651ebc8eb900d824a2d9d42b069415172
Stergios Bachoumas <stergiosba@gmail.com>
2022-05-27 14:58:08 +0300

    test gyro bias offset

--- test_spi/main.c
+++ test_spi/main.c
@@ -7,6 +7,7 @@
 float32_t xg=0;
 float32_t yg=0;
 float32_t zg=0;
+
 char buffer[128];
 uint16_t imu_check;
 int main(void)
@@ -21,12 +22,14 @@ int main(void)
     ERTM;
 
     imu_check=IMUCommsCheck();
+    //SPI_writeDataBlockingNonFIFO(SUS_SPI_BASE, 0xBE01);
 
     while(1)
     {
+
         //xg = RawToReal(SensorRead(XACCL_OUT, SBITS14), g_AcclScale,SBITS14, 1);
         //yg = RawToReal(SensorRead(YACCL_OUT, SBITS14), g_AcclScale,SBITS14, 1);
-        zg = RawToReal(SensorRead(ZACCL_OUT, SBITS14), g_AcclScale,SBITS14, 1);
+        yg = RawToReal(SensorRead(YGYRO_OUT, SBITS14), g_GyroScale, SBITS14, 1);
         //sprintf(buffer,"%d\r\n",(int16_t)xg);
         //SCI_writeCharArray(SCIA_BASE, (uint16_t*)buffer, 128);
 
